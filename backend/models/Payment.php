f167c28bc67e5d0ced404ceba6b814a58c2eac68
dan82 <ed@easy-sms.ru>
2017-05-23 15:46:24 +0700

    save negative values in Outgoig Payment

--- backend/models/Payment.php
+++ backend/models/Payment.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace backend\models;
 
 use yii\db\ActiveRecord;
 
-class Payment extends ActiveRecord
-{
+class Payment extends ActiveRecord{
   const STATUS_NEW        = 1;
   const STATUS_CONFIRMED  = 2;
   const STATUS_DELETED    = 3;
@@ -13,16 +11,14 @@ class Payment extends ActiveRecord
   /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName(){
         return 'payments';
     }
     
     /**
      * @return Car model find by car_id
      */
-    public function getCar()
-    {
+    public function getCar(){
       $car = Car::findOne(['id'=>$this->car_id]);
       if (!$car)
         $car = new Car();
@@ -30,12 +26,11 @@ class Payment extends ActiveRecord
       return $car;
     }
     
-    public static function getListStatuses($with_all = false)
-    {
+    public static function getListStatuses($with_all = false){
       if ($with_all)
         return [0=>'ALL', self::STATUS_NEW=>'New', self::STATUS_CONFIRMED=>'Confirmed', self::STATUS_DELETED=>'Deleted', self::STATUS_UNPAID=>'Unpaid'];
-      
-      return [self::STATUS_NEW=>'New', self::STATUS_CONFIRMED=>'Confirmed', self::STATUS_DELETED=>'Deleted', self::STATUS_UNPAID=>'Unpaid'];
+      else
+        return [self::STATUS_NEW=>'New', self::STATUS_CONFIRMED=>'Confirmed', self::STATUS_DELETED=>'Deleted', self::STATUS_UNPAID=>'Unpaid'];
     }
     
     public function getStatusName(){
@@ -49,5 +44,17 @@ class Payment extends ActiveRecord
         default:
           return 'Unknown';
       }
+    }
+    
+    public function beforeSave($insert){
+      if ($this->type_id == PaymentType::OUTGOING) $s = -1;
+      else $s = 1;
+        
+      $this->usd    = $s * abs($this->usd);
+      $this->euro   = $s * abs($this->euro);
+      $this->thb    = $s * abs($this->thb);
+      $this->ruble  = $s * abs($this->ruble);
+      
+      return parent::beforeSave($insert);
     }
 }
